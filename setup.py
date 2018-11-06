4b823b90f8bff3cea9faa634cb01bb3da6fa510b
James W. McClain <jmcclain@azavea.com>
2018-11-06 12:40:36 -0500

    Update Dependencies

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ from setuptools import setup
 setup(
     name='mask-to-polygons',
     version='0.1.0',
-    description='Routines for extracting polygons from semantic segmentation masks',
+    description='Routines for extracting and working with polygons from semantic segmentation masks',
     url='https://github.com/jamesmcclain/mask-to-polygons',
     author='James McClain',
     author_email='jmcclain@azava.com',
     license='Apache License 2.0',
     install_requires=[
         'Shapely==1.6.*',
+        'opencv-python==3.4.*',
+        'numpy>=1.0.0',
+        'rasterio>=1.0.0',
     ],
     packages=[
         'mask_to_polygons',
