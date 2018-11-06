f62a280ffa7ec29e7bebfea0a2917978359ef26c
James W. McClain <jmcclain@azavea.com>
2018-11-06 12:51:47 -0500

    Code for Buildings

---
+++ mask_to_polygons/processing/buildings.py
@@ -0,0 +1,81 @@
+import cv2
+import json
+import numpy as np
+
+
+def geometries_from_geojson(filename):
+    geojson = None
+    gs = []
+
+    with open(filename, 'r') as file:
+        geojson = json.loads(file.read())
+    for g in geojson['features']:
+        gs.append(g['geometry'])
+
+    return gs
+
+
+def get_rectangle(buildings):
+    _, contours, _ = cv2.findContours(buildings.copy(), cv2.RETR_EXTERNAL,
+                                      cv2.CHAIN_APPROX_SIMPLE)
+    rotrect = cv2.minAreaRect(contours[0])
+    return rotrect
+
+
+def get_kernel(buildings,
+               min_aspect_ratio=1.618,
+               width_factor=0.75,
+               rotrect=None):
+    if rotrect is None:
+        rotrect = get_rectangle(buildings)
+    (_, (xwidth, ywidth), angle) = rotrect
+    width = int(width_factor * min(xwidth, ywidth))
+    kernel = np.zeros((width, width), dtype=np.uint8)
+    try:
+        kernel = cv2.cvtColor(kernel, cv2.COLOR_GRAY2BGR)
+    except Exception:
+        return None
+    ratio = max(ywidth, xwidth) / min(ywidth, xwidth)
+    if ratio < min_aspect_ratio:
+        return None
+    if ywidth > xwidth:
+        element = (((width // 2, width // 2), (width, 1), angle))
+    else:
+        element = (((width // 2, width // 2), (width, 1), angle + 90))
+    element = cv2.boxPoints(element)
+    element = np.int0(element)
+    cv2.drawContours(kernel, [element], 0, (1, 0, 0), -1)
+    kernel = kernel[:, :, 0]
+    return kernel
+
+
+def split_buildings(buildings,
+                    min_aspect_ratio=1.618,
+                    min_area_ratio=0.75,
+                    max_steps=10):
+    buildings_area = float(buildings.sum())
+    retval = buildings
+    low = 0.0
+    high = 1.0
+    i = 0
+
+    while i < max_steps:
+        width_factor = (low + high) / 2.0
+        kernel = get_kernel(
+            buildings,
+            min_aspect_ratio=min_aspect_ratio,
+            width_factor=width_factor)
+        if kernel is None:
+            return buildings
+        _retval = cv2.morphologyEx(
+            buildings.copy(), cv2.MORPH_OPEN, kernel, iterations=1)
+        _retval = np.array(_retval == 1, dtype=np.uint8)
+        split_area = float(_retval.sum())
+        if split_area / buildings_area > min_area_ratio:
+            low = width_factor
+            retval = _retval
+        else:
+            high = width_factor
+        i = i + 1
+
+    return retval
